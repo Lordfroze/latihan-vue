0d8322ba35928d95bfe3d7c3b425684ccb5bdbea
yoga <yogaadipratama22@gmail.com>
2025-10-05 18:41:14 +0700

    counter done

--- vue-counter/src/App.vue
+++ vue-counter/src/App.vue
@@ -18,10 +18,10 @@ const count = ref(1);
     <main>
         <div class="container py-5">
             <h1 class="text-5xl font-bold text-center">Counter App</h1>
-            <p id="counter" class="text-5xl font-bold text-center py-10">0</p>
+            <p id="counter" class="text-5xl font-bold text-center py-10">{{count}}</p>
             <div class="flex justify-center items-center gap-4">
-                <button id="increment" class="bg-sky-500 hover:bg-sky-700  rounded-2xl text-white px-8 py-4">+</button>
-                <button id="decrement" class="bg-red-500 hover:bg-red-700 rounded-2xl text-white px-8 py-4">-</button>
+                <button id="increment" @click="count++" class="bg-sky-500 hover:bg-sky-700  rounded-2xl text-white px-8 py-4">+</button>
+                <button id="decrement" @click="count--" class="bg-red-500 hover:bg-red-700 rounded-2xl text-white px-8 py-4">-</button>
             </div>
             <p class="text-center italic text-sm py-5 px-1.5">Click the button to increase or decrease the counter</p>
         </div>
