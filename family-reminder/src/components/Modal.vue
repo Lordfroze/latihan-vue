baeafda6d9f6454a2855278feef5ad6f25b252bb
yoga <yogaadipratama22@gmail.com>
2025-10-10 11:13:31 +0700

    add list card

--- family-reminder/src/components/Modal.vue
+++ family-reminder/src/components/Modal.vue
@@ -1,160 +1,55 @@
 <script setup>
-  import { ref } from "vue";
-  const showForm = ref(false); // state untuk menampilkan dan menyembunyikan form modal
+import { ref } from "vue";
+const showForm = ref(false); // state untuk menampilkan dan menyembunyikan form modal
 </script>
 
 <template>
-    <div class="fab">
-  <!-- a focusable div with tabindex is necessary to work on all browsers. role="button" is necessary for accessibility -->
-  <div tabindex="0" role="button" class="btn btn-lg btn-circle btn-primary" @click="showForm = true">+</div>
-
-  <!-- buttons that show up when FAB is open -->
-  <button class="btn btn-lg btn-circle">Add</button>
-</div>
-
   <main>
-    <!-- memo -->
-    <div class="container">
-      <header>
-        <h1 class="header-title">Memo</h1>
-        <button @click="showForm = true" class="header-button">+</button>
-        <!-- tombol tambah memo untuk menampilkan form -->
-      </header>
-      <!-- card memo -->
-      <div class="card-container">
-        <div class="card">
-          <p class="card-content">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam totam
-            eveniet harum dolores, nihil maiores delectus earum quod non tempore
-            dolor a rerum error rem animi? Aspernatur id ipsum totam.
-          </p>
-          <p class="card-date">2023-08-20</p>
-        </div>
+    <!-- Daisy ui + button -->
+    <div class="fab">
+      <div tabindex="0" role="button" class="btn btn-lg btn-circle btn-primary" @click="showForm = true">+</div>
+      <button class="btn btn-lg btn-circle" onclick="my_modal_1.showModal()">Add</button>
+    </div>
+    <!-- end Daisy ui + button -->
 
-        <div class="card">
-          <p class="card-content">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam totam
-            eveniet harum dolores, nihil maiores delectus earum quod non tempore
-            dolor a rerum error rem animi? Aspernatur id ipsum totam.
-          </p>
-          <p class="card-date">2023-08-20</p>
+    <!-- daisy ui modal -->
+    <!-- Open the modal using ID.showModal() method -->
+    <dialog id="my_modal_1" class="modal">
+      <div class="modal-box">
+        <h3 class="text-lg font-bold">Hello!</h3>
+        <textarea class="textarea" placeholder="Bio"></textarea>
+        <div class="modal-action">
+          <form method="dialog">
+            <!-- if there is a button in form, it will close the modal -->
+            <button class="btn">Close</button>
+          </form>
         </div>
       </div>
-      <!--end card memo -->
-    </div>
-    <!-- end memo -->
+    </dialog>
+    <!-- end daisy ui modal -->
 
-    <!-- form input memo / form modal -->
-    <div v-show="showForm" class="form-overlay">
-      <div class="form-modal">
-        <button @click="showForm = false" class="form-close-btn">
-          &times;
-        </button>
-        <!-- tombol close form untuk menyembunyikan form -->
-        <textarea name="memo" id="memo" cols="30" rows="10"></textarea>
-        <button class="form-save-btn">Save</button>
+    <!-- daisy ui card-->
+    <div class="flex flex-wrap gap-2 justify-center">
+      <div class="card card-border bg-base-100 w-96">
+        <div class="card-body">
+          <h2 class="card-title">Card Title</h2>
+          <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">View</button>
+          </div>
+        </div>
       </div>
-    </div>
-    <!-- end form input memo / end form modal -->
+
+      <div class="card card-border bg-base-100 w-96">
+        <div class="card-body">
+          <h2 class="card-title">Card Title</h2>
+          <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">View</button>
+          </div>
+        </div>
+      </div>
+      </div>
+    <!-- end daisy ui card -->
   </main>
 </template>
-
-<!-- style card memo -->
-<style scoped>
-  main {
-    height: 100vh;
-    width: 100vh;
-  }
-
-  .container {
-    max-width: 900px;
-    padding: 10px;
-    margin: 0 auto;
-  }
-
-  header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-
-  .header-title {
-    font-size: 48px;
-    font-weight: bold;
-    margin-bottom: 25px;
-    color: #495a7d;
-  }
-
-  .header-button {
-    border: none;
-    padding: 10px;
-    width: 50px;
-    height: 50px;
-    cursor: pointer;
-    border-radius: 100%;
-    background-color: #495a7d;
-    color: white;
-  }
-
-  .card-container {
-    display: flex;
-    flex-wrap: wrap;
-    gap: 20px;
-  }
-
-  .card {
-    width: 225px;
-    height: 225px;
-    padding: 10px;
-    background-color: #6e54cc;
-    margin-bottom: 20px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-  }
-
-  .form-overlay {
-    position: absolute; /** agar posisi diatas semua elemen */
-    top: 0; /** posisi dari atas */
-    left: 0; /** posisi dari kiri */
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.77);
-    z-index: 10;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-
-  .form-modal {
-    width: 420px;
-    background-color: white;
-    border-radius: 10px;
-    padding: 30px;
-    position: relative;
-    display: flex;
-    flex-direction: column;
-  }
-
-  .form-save-btn {
-    padding: 10px 20px;
-    font-size: 20px;
-    width: 100%;
-    background-color: #495a7d;
-    border: none;
-    cursor: pointer;
-    border-radius: 5px;
-    margin-top: 15px;
-    color: white;
-  }
-
-  .form-close-btn {
-    position: absolute;
-    top: 10px;
-    right: 10px;
-    font-size: 20px;
-    border: none;
-    background-color: transparent;
-    cursor: pointer;
-  }
-</style>
--- family-reminder/src/views/List.vue
+++ family-reminder/src/views/List.vue
@@ -9,8 +9,5 @@ import Nav from '@/components/Nav.vue';
     <main class="px-2 item-center">
         <h1 class="text-3xl text-center">Halaman List</h1>
         <Modal />
-        <div class="flex flex-wrap gap-2 justify-center">
-        </div>
-
     </main>
 </template>
