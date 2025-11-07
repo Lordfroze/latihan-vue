39bc6b6dc3db6a88b5269e9fe4d11d0da3b9f42b
lordfroze <yogaadipratama22@gmail.com>
2025-11-07 13:11:21 +0700

    Membuat Endpoint untuk Mendapatkan Satu Data Product

--- vuestore-server/app/controllers/product.controller.js
+++ vuestore-server/app/controllers/product.controller.js
@@ -12,3 +12,18 @@ exports.findAll = (req, res) => {
             });
         });
 }
+
+// fungsi menampilkan 1 product berdasarkan id
+exports.findOne = (req, res) => {
+    Product.findOne({
+        code : req.params.id // mengambil id dari params 
+    })
+        .then(result => {
+            res.send(result);
+        })
+        .catch(err => {
+            res.status(409).send({
+                message: err.message || "Gagal mengambil data product"
+            });
+        });
+}
--- vuestore-server/app/routes/product.route.js
+++ vuestore-server/app/routes/product.route.js
@@ -2,7 +2,9 @@ module.exports = (app) => {
     const products = require('../controllers/product.controller');
     const router = require('express').Router();
 
-    router.get('/', products.findAll);
+    
+    router.get('/', products.findAll); // route untuk mengambil semua data product
+    router.get('/:id', products.findOne); // route untuk mengambil 1 data product berdasarkan id
 
     app.use('/api/products', router);
 }
